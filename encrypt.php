d750e995024eecab8e2a55f5953d377eb4a5a809
Seb <Sebastianmuchui79@gmail.com>
2022-04-24 08:19:35 +0300

    init.................

---
+++ encrypt.php
@@ -0,0 +1,55 @@
+<?php
+$str = "string";
+$number = 5;
+function encrypt_base64_layers($str,$number){
+   for($i=0;  $i<$number; $i++){
+      $str = base64_encode($str);
+      
+         if ($i == $number-1){
+            print $str."<br>";
+         }
+
+      
+   }
+}
+function encrypt_hex_bin_layers($str,$number){
+   for($i=0;  $i<$number; $i++){
+      $str = bin2hex($str);
+      
+         if ($i == $number-1){
+            print $str."<br>";
+         }
+
+      
+   }
+}
+
+function decrypt_base64_layers($str,$number){
+   for($i=0;  $i<$number; $i++){
+      $str = base64_decode($str);
+      
+         if ($i == $number-1){
+            print $str."<br>";
+         }
+
+      
+   }
+}
+
+function decrypt_bin_hex_layers($str,$number){
+   for($i=0;  $i<$number; $i++){
+      $str = base64_decode($str);
+      
+         if ($i == $number-1){
+            print $str."<br>";
+         }
+
+      
+   }
+}
+
+
+?>
+<body style="background-color: black; color: firebrick; font-family: monospace; font-size: 15px;">
+   
+</body>
